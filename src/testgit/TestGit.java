043e694508e8e9f960923124452c5435a6b9309f
ke <ke@10.2.110.156>
2016-05-05 09:36:27 +0200

    Conflict Huzva vs. Riha resolved

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -6,6 +6,7 @@ public class TestGit {
         System.out.println("Seznam naší třídy:");
         System.out.println("René Hužva");
         System.out.println("Jaroslav Říha");
+        System.out.println("René Hužva");
     }
     
 }
