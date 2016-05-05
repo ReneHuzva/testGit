92359c66da18bbe834c59b21f065f1044eef0cb0
ke <ke@10.2.110.156>
2016-05-05 10:01:41 +0200

    Pimprloch added

--- src/testgit/TestGit.java
+++ src/testgit/TestGit.java
@@ -7,5 +7,6 @@ public class TestGit {
         System.out.println("René Hužva");
         System.out.println("Jaroslav Říha");    
         System.out.println("Martin Hencl");
+        System.out.println("Pimprloch");
     }
 }
